7ccfd4083d96b863e368ca1a268d003d42839260
foxbuka <krushenitskii@gmail.com>
2019-08-04 12:52:14 +0300

    new filtr of price

--- public/js/render.js
+++ public/js/render.js
@@ -1,6 +1,7 @@
-let type = ''
 
+let type = '',data,seller;
 let usualSort = document.getElementById('usual');
+
 usualSort.addEventListener('click', () =>{
     type = 'all'
     Render(type);
@@ -11,29 +12,25 @@ function getType(btn)
         type = btn;
         Render(type);
 }
-
-
-async function Render(type) {
+function renderPage(data) {
     const row = document.getElementById('row');
-    const response =  await fetch("https://avito.dump.academy/products");
-    let data = await response.json();
-    data = data.data;
-    const sellers = await fetch("https://avito.dump.academy/sellers");
-    let seller= await sellers.json();
-    seller = seller.data;
-    let finishdata = ``;
-
-    let priceSort = document.getElementById("priceSort");
-    let raitingSort = document.getElementById('raitingSort');
     
-    raitingSort.addEventListener('click', () => {
-        finishdata = '';
-       data = data.sort((a,b)=> seller[b.relationships.seller].rating - seller[a.relationships.seller].rating);
-
-       for ( let i = 0; i < data.length; i++){
+    let finishdata = ``;
+    if(data.length === 0 )
+    {
+      finishdata += `<div class="container">
+          <div class="warning">
+                <h4 class="center"> По вашему запросу ничего не найдено </h4>
+                <p > <h5 class="center"> Пожалуйста введите данные верно!</h5> </p>
+          </div>
+        </div>` 
+    }
+    else {
+    for ( let i = 0; i < data.length; i++){
         if(data[i].price !== undefined) {
             if(type === '' || type==="all"){
              finishdata += `<div class="col s12 m4">
+            
              <div class="card">
   
                    <div class="card-image">
@@ -46,13 +43,10 @@ async function Render(type) {
                      <p>
                          <span class="icons"> <i class="fas fa-star">  ${seller[data[i].relationships.seller].rating}</i> </span> 
                          <span class="icons"><i class="fas fa-camera-retro">  ${data[i].pictures.length}</i></span>
-                         <span class="icons"><i class="fas fa-heart add-like"></i></span>
+                         <span class="icons"><button class="favorite" onclick="her(this.value)" value='${data[i]}'> <i class="fas fa-heart add-like"></i> </button></span> 
                      </p>
                    </div>
-                   <div class="card-reveal">
-                   <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                   <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                   </div>
+                   
                 </div>
              </div>`
             } else if(type === data[i].category)
@@ -73,153 +67,102 @@ async function Render(type) {
                          <span class="icons"><i class="fas fa-heart add-like"></i></span>
                      </p>
                    </div>
-                   <div class="card-reveal">
-                   <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                   <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                   </div>
                 </div>
              </div>`
             }
            
         }
      }
+    }
+     
      row.innerHTML = finishdata;
-    // console.log('dataa of sallers')
-    // for(let i = 0; i < seller.length; i++)
-    // {
-    //     console.log(seller[i].id);
-    // }
+}
 
-    // console.log('My arr')
-    // for(let i = 0; i < arr.length; i++)
-    // {
-    //     console.log(arr[i].relationships.seller);
-    // }
+function her(data)
+{
+  createDb(data);
+  
+}
+// function createDb(data){
+//   const request = indexedDB.open('favorites');
 
-    console.log(seller)
-    console.log(arr);
-      
-    })
+
+//       request.onupgradeneeded = e => {
+//         const db = e.target.result;
+//         // favorites = {
+//         //   data:data 
+//             // id:id
+//         // }
+//         db.createObjectStore('personal_fav', {keyPath:"id"});
+//           alert("Upgrade");
+//       }
+  
+//       request.onsuccess = e => {
+//         const db = e.target.result;
+//           alert(`Success  ${db.name}`);
+//       }
+  
+//       request.onerror = e => {
+//           alert("Error");
+//       }
+// }
+async function Render(type) {
+    const response =  await fetch("https://avito.dump.academy/products");
+    data = await response.json();
+    data = data.data;
+
+    const sellers = await fetch("https://avito.dump.academy/sellers");
+    seller= await sellers.json();
+    seller = seller.data;
+
+
+    const priceSort = document.getElementById("priceSort");
+    const raitingSort = document.getElementById('raitingSort');
+ 
+    const range = document.getElementById('range');
+    document.getElementById("min").addEventListener('input', restrictToInteger);
+    function restrictToInteger() {
+        this.value = this.value.replace(/[^\d]/g, '');
+    }
+
+    document.getElementById("max").addEventListener('input', restrictToInteger);
+    function restrictToInteger() {
+        this.value = this.value.replace(/[^\d]/g, '');
+    }
+
+      range.addEventListener('click', () => {
+        const min = parseInt(document.getElementById('min').value);
+        const max = parseInt(document.getElementById('max').value);
+        
+        const arr = [];
+        console.log(data.length)
+         for(let i = 0; i < data.length; i++)
+         {
+           if(data[i].price >= min && data[i].price <=max)
+           {
+            arr.push(data[i]);
+           }
+          
+         }
+        renderPage(arr);
+      })
+
+    raitingSort.addEventListener('click', () => {
+
+        data.sort((a,b)=> seller[b.relationships.seller].rating - seller[a.relationships.seller].rating);
+
+        renderPage(data);
+    });
+
     priceSort.addEventListener("click", ()=>{
-    finishdata = ``;
        data = data.sort(function(obj1,obj2){
               return obj1.price - obj2.price;
         });
 
-        for ( let i = 0; i < data.length; i++){
-            if(data[i].price !== undefined) {
-                if(type === '' || type==="all"){
-                 finishdata += `<div class="col s12 m4">
-                 <div class="card">
-      
-                       <div class="card-image">
-                       <img class="activator adv-img" src="${data[i].pictures[0]}">
-                       <span class="card-title">${seller[data[i].relationships.seller].name}</span>
-                       </div>
-                       <div class="card-content">
-                       <span class="title"> <a href="#"> ${data[i].title}</a></span>
-                       <p class="price"> ${data[i].price.toLocaleString('ru')} <i class="fas fa-ruble-sign"></i></p>
-                         <p>
-                             <span class="icons"> <i class="fas fa-star">  ${seller[data[i].relationships.seller].rating}</i> </span> 
-                             <span class="icons"><i class="fas fa-camera-retro">  ${data[i].pictures.length}</i></span>
-                             <span class="icons"><i class="fas fa-heart add-like"></i></span>
-                         </p>
-                       </div>
-                       <div class="card-reveal">
-                       <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                       <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                       </div>
-                    </div>
-                 </div>`
-                } else if(type === data[i].category)
-                {
-                 finishdata += `<div class="col s12 m4">
-                 <div class="card">
-      
-                       <div class="card-image">
-                       <img class="activator adv-img" src="${data[i].pictures[0]}">
-                       <span class="card-title">${seller[data[i].relationships.seller].name}</span>
-                       </div>
-                       <div class="card-content">
-                       <span class="title"> <a href="#"> ${data[i].title}</a></span>
-                       <p class="price"> ${data[i].price.toLocaleString('ru')} <i class="fas fa-ruble-sign"></i></p>
-                         <p>
-                             <span class="icons"> <i class="fas fa-star">  ${seller[data[i].relationships.seller].rating}</i> </span> 
-                             <span class="icons"><i class="fas fa-camera-retro">  ${data[i].pictures.length}</i></span>
-                             <span class="icons"><i class="fas fa-heart add-like"></i></span>
-                         </p>
-                       </div>
-                       <div class="card-reveal">
-                       <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                       <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                       </div>
-                    </div>
-                 </div>`
-                }
-               
-            }
-         }
-         
-         row.innerHTML = finishdata;
-    
+        renderPage(data);
     });
-   
-    for ( let i = 0; i < data.length; i++){
-       if(data[i].price !== undefined) {
-           if(type === '' || type==="all"){
-            finishdata += `<div class="col s12 m4">
-            <div class="card">
- 
-                  <div class="card-image">
-                  <img class="activator adv-img" src="${data[i].pictures[0]}">
-                  <span class="card-title">${seller[data[i].relationships.seller].name}</span>
-                  </div>
-                  <div class="card-content">
-                  <span class="title"> <a href="#"> ${data[i].title}</a></span>
-                  <p class="price"> ${data[i].price.toLocaleString('ru')} <i class="fas fa-ruble-sign"></i></p>
-                    <p>
-                        <span class="icons"> <i class="fas fa-star">  ${seller[data[i].relationships.seller].rating}</i> </span> 
-                        <span class="icons"><i class="fas fa-camera-retro">  ${data[i].pictures.length}</i></span>
-                        <span class="icons"><i class="fas fa-heart add-like"></i></span>
-                    </p>
-                  </div>
-                  <div class="card-reveal">
-                  <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                  <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                  </div>
-               </div>
-            </div>`
-           } else if(type === data[i].category)
-           {
-            finishdata += `<div class="col s12 m4">
-            <div class="card">
- 
-                  <div class="card-image">
-                  <img class="activator adv-img" src="${data[i].pictures[0]}">
-                  <span class="card-title">${seller[data[i].relationships.seller].name}</span>
-                  </div>
-                  <div class="card-content">
-                  <span class="title"> <a href="#"> ${data[i].title}</a></span>
-                  <p class="price"> ${data[i].price.toLocaleString('ru')} <i class="fas fa-ruble-sign"></i></p>
-                    <p>
-                        <span class="icons"> <i class="fas fa-star">  ${seller[data[i].relationships.seller].rating}</i> </span> 
-                        <span class="icons"><i class="fas fa-camera-retro">  ${data[i].pictures.length}</i></span>
-                        <span class="icons"><i class="fas fa-heart add-like"></i></span>
-                    </p>
-                  </div>
-                  <div class="card-reveal">
-                  <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                  <p>Here is some more information about this product that is only revealed once clicked on.</p>
-                  </div>
-               </div>
-            </div>`
-           }
-          
-       }
-    }
     
-    row.innerHTML = finishdata;
+    renderPage(data);
  }
  
  Render(type);
- 
